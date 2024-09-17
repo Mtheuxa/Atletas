bf54478c73da4464cb89be8f9f051321619d1d80
Mtheuxa <matheusaraujopaulucci@gmail.com>
2024-09-17 07:13:41 -0300

    Add files via upload
    Códigos

---
+++ JavaScript/dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Neymar",
+        descricao: "Neymar da Silva Santos Júnior, mais conhecido como Neymar. Revelado nas categorias de base do Santos, logo se destacou por sua habilidade, dribles e gols espetaculares. Sua carreira o levou a grandes clubes como Barcelona e Paris Saint-Germain, onde conquistou diversos títulos e se tornou um ícone mundial do esporte.",
+        link: "https://pt.wikipedia.org/wiki/Neymar",
+        tags: "njr ney"
+    },
+    {
+        titulo: "Messi",
+        descricao: "Lionel Messi, considerado por muitos o maior jogador de futebol de todos os tempos, é um craque argentino que encanta o mundo com sua habilidade única e seus gols espetaculares. Com uma carreira brilhante marcada por títulos e recordes, Messi se tornou um ícone do esporte e uma inspiração para milhões de fãs ao redor do globo.",
+        link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+        tags: "messi"
+    },
+    {
+        titulo: "Cristiano Ronaldo",
+        descricao: "Cristiano Ronaldo dos Santos Aveiro, é um futebolista português que se consagrou como um dos maiores jogadores de todos os tempos. Conhecido por sua habilidade excepcional, velocidade e gols espetaculares, Ronaldo conquistou inúmeros títulos e prêmios ao longo de sua carreira, representando clubes como Sporting,Manchester United, Real Madrid e Juventus.",
+        link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+        tags: "Cr7 cristiano cris robo"
+    },
+    {
+        titulo: "Vini Jr",
+        descricao: "Vinícius Júnior é um dos maiores talentos do futebol brasileiro da atualidade. Atacante veloz e habilidoso, ele conquistou o mundo com suas jogadas brilhantes e dribles desconcertantes. Revelado nas categorias de base do Flamengo, foi contratado pelo Real Madrid ainda muito jovem e se tornou peça fundamental na equipe merengue.",
+        link:"https://pt.wikipedia.org/wiki/Vinícius_Júnior",
+        tags: "vini Vini jr"
+    },
+    {
+        titulo:"Rodrygo",
+        descricao:"Rodrygo Silva de Goes, mais conhecido como Rodrygo, é um jovem e talentoso jogador de futebol brasileiro que deslumbra os campos europeus com sua habilidade e técnica refinada. Revelado pelo Santos FC, rapidamente se destacou e logo foi contratado pelo Real Madrid, um dos maiores clubes do mundo. Com sua velocidade, dribles desconcertantes e finalização precisa, Rodrygo se tornou um dos principais jogadores da equipe merengue e da Seleção Brasileira, prometendo um futuro brilhante no futebol mundial.",
+        link:"https://pt.wikipedia.org/wiki/Rodrygo",
+        tags:"rodry goes"
+    }
+];
